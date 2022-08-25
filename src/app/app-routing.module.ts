156e40a6397984c0890f6e9c188ff5d2292f2e3d
Lionel <lionelx87@gmail.com>
2022-08-24 22:22:15 -0300

    add: login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './modules/auth/auth.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/docs/components.md
@@ -0,0 +1,12 @@
+# NewRoute (Backoffice) :rocket:
+---
+
+##### Create modules in the project with child route file :monorail::
+
+`ng g module modules/auth --routing` // Create the auth module
+
+##### Create pages in the project :page_facing_up::
+
+`ng g c modules/auth/pages/login` // Create login page
+
+
