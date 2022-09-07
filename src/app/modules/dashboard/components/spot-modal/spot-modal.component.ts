1e9a328b2f9f1be17dc4e108e143602702432c61
Lionel <lionelx87@gmail.com>
2022-09-07 00:07:11 -0300

    feat: send data to backend

--- src/app/modules/dashboard/components/spot-modal/spot-modal.component.ts
+++ src/app/modules/dashboard/components/spot-modal/spot-modal.component.ts
@@ -7,20 +7,23 @@ import { SpotService } from 'src/app/modules/shared/services/spot.service';
 @Component({
   selector: 'app-spot-modal',
   templateUrl: './spot-modal.component.html',
-  styleUrls: ['./spot-modal.component.scss']
+  styleUrls: ['./spot-modal.component.scss'],
 })
 export class SpotModalComponent implements OnInit {
   @ViewChild('myPond') myPond!: FilePond;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private spotService: SpotService) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private spotService: SpotService
+  ) {}
 
   pondOptions: FilePondOptions = {
     allowMultiple: true,
     labelIdle: 'Arrastrá o selecciona las imágenes...',
     // acceptedFileTypes: ['image/jpeg, image/png', 'image/jpg'],
-    allowReorder:true,
-    maxFiles:5,
-  }
+    allowReorder: true,
+    maxFiles: 5,
+  };
 
   spotForm = new FormGroup({
     category_id: new FormControl('', [Validators.required]),
@@ -31,12 +34,10 @@ export class SpotModalComponent implements OnInit {
     address: new FormControl('', [Validators.required]),
     latitude: new FormControl('-46.435352'),
     longitude: new FormControl('-67.521189'),
-    phones: new FormControl('')
+    phones: new FormControl(''),
   });
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   get isInvalid() {
     return this.spotForm.invalid;
@@ -55,30 +56,38 @@ export class SpotModalComponent implements OnInit {
   }
 
   pondHandleActivateFile(event: any) {
-    console.log('A file was activated', event)
+    console.log('A file was activated', event);
   }
 
-  uploadFiles(){
+  uploadFiles() {
     console.log(this.myPond.getFiles());
   }
 
   save() {
-    if(this.spotForm.valid) {
+    if (this.spotForm.valid) {
       const form = this.spotForm.value;
+      form.images = this.slugify(this.spotForm.controls['name_es'].value);
       const formData = new FormData();
-      for(const key of Object.keys(form)) {
+      for (const key of Object.keys(form)) {
         const value = form[key];
         formData.append(key, value);
       }
       // Images
-      const files = this.myPond.getFiles().map(elem => elem.file);
-      for(const image of files){
+      const files = this.myPond.getFiles().map((elem) => elem.file);
+      for (const image of files) {
         formData.append('files[]', image);
       }
-      this.spotService.spotCreate(formData).subscribe( (resp) => {
+      this.spotService.spotCreate(formData).subscribe((resp) => {
         console.log('respuesta: ', resp);
       });
     }
   }
 
+  private slugify = (str: string) =>
+    str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
 }
